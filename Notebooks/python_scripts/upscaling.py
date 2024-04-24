1a59b292a40124b2b65f9c31540d089f974f2cd9
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2024-04-24 11:25:44 +0100

    added np.squeeze to avoid extra dims after upscaling

--- Notebooks/python_scripts/upscaling.py
+++ Notebooks/python_scripts/upscaling.py
@@ -1,9 +1,17 @@
 import numpy as np
 
-from nanopyx.core.transform._le_interpolation_catmull_rom import ShiftAndMagnify as interp_cr
-from nanopyx.core.transform._le_interpolation_catmull_rom import ShiftScaleRotate as magnify_cr
-from nanopyx.core.transform._le_interpolation_nearest_neighbor import ShiftAndMagnify as interp_nn
-from nanopyx.core.transform._le_interpolation_nearest_neighbor import ShiftScaleRotate as magnify_nn
+from nanopyx.core.transform._le_interpolation_catmull_rom import (
+    ShiftAndMagnify as interp_cr,
+)
+from nanopyx.core.transform._le_interpolation_catmull_rom import (
+    ShiftScaleRotate as magnify_cr,
+)
+from nanopyx.core.transform._le_interpolation_nearest_neighbor import (
+    ShiftAndMagnify as interp_nn,
+)
+from nanopyx.core.transform._le_interpolation_nearest_neighbor import (
+    ShiftScaleRotate as magnify_nn,
+)
 
 
 def upsample_img(img: np.array, magnification: int, keep_dims: bool = False):
@@ -15,29 +23,24 @@ def upsample_img(img: np.array, magnification: int, keep_dims: bool = False):
     """
     if keep_dims:
         cr_upscale = magnify_cr()
-        return np.asarray(
-            cr_upscale.run(
-                img.astype(np.float32),
-                0,
-                0,
-                magnification,
-                magnification,
-                0
+        return np.squeeze(
+            np.asarray(
+                cr_upscale.run(
+                    img.astype(np.float32), 0, 0, magnification, magnification, 0
                 ),
-            dtype=np.float32
+                dtype=np.float32,
             )
+        )
     else:
         cr_upscale = interp_cr()
-        return np.asarray(
-            cr_upscale.run(
-                img.astype(np.float32),
-                0,
-                0,
-                magnification,
-                magnification
+        return np.squeeze(
+            np.asarray(
+                cr_upscale.run(
+                    img.astype(np.float32), 0, 0, magnification, magnification
                 ),
-            dtype=np.float32
+                dtype=np.float32,
             )
+        )
 
 
 def upsample_labels(labels: np.array, magnification: int, keep_dims: bool = False):
@@ -49,26 +52,21 @@ def upsample_labels(labels: np.array, magnification: int, keep_dims: bool = Fals
     """
     if keep_dims:
         nn_upscale = magnify_nn()
-        return np.asarray(
-            nn_upscale.run(
-                labels.astype(np.float32),
-                0,
-                0,
-                magnification,
-                magnification,
-                0
+        return np.squeeze(
+            np.asarray(
+                nn_upscale.run(
+                    labels.astype(np.float32), 0, 0, magnification, magnification, 0
                 ),
-            dtype=np.float32
+                dtype=np.float32,
             )
+        )
     else:
         nn_upscale = interp_nn()
-        return np.asarray(
-            nn_upscale.run(
-                labels.astype(np.float32),
-                0,
-                0,
-                magnification,
-                magnification
+        return np.squeeze(
+            np.asarray(
+                nn_upscale.run(
+                    labels.astype(np.float32), 0, 0, magnification, magnification
                 ),
-            dtype=np.float32
+                dtype=np.float32,
             )
+        )
